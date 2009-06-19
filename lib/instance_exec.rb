4dd4d17a8144c6198b5dacc9fc80f51c7dec3e79
mccraig mccraig of the clan mccraig <craig@trampolinesystems.com>
2009-06-19 17:32:43 +0100

    made instance_exec impl threadsafe for ruby 1.8.6

--- lib/instance_exec.rb
+++ lib/instance_exec.rb
@@ -1,24 +1,34 @@
 # defines Object.instance_exec to permit call of a Proc with params
 # in the context of an instance : instance.instance_exec( foo, bar, &proc )
-
-unless defined? instance_exec # 1.8.7, 1.9
-  class Proc #:nodoc:
-    # defines an UnboundMethod, and binds it to object
-    def bind(object)
-      block, time = self, Time.now
-      (class << object; self end).class_eval do
-        method_name = "__bind_#{time.to_i}_#{time.usec}"
-        define_method(method_name, &block)
-        method = instance_method(method_name)
-        remove_method(method_name)
-        method
-      end.bind(object)
+# taken from rails 2.2
+#
+class Object
+  unless defined? instance_exec # 1.9
+    module InstanceExecMethods #:nodoc:
+      @mutex = Mutex.new
+      class << self
+        attr_reader :mutex
+      end
     end
-  end
+    include InstanceExecMethods
 
-  class Object
-    def instance_exec(*arguments, &block)
-      block.bind(self)[*arguments]
+    # Evaluate the block with the given arguments within the context of
+    # this object, so self is set to the method receiver.
+    #
+    # From Mauricio's http://eigenclass.org/hiki/bounded+space+instance_exec
+    def instance_exec(*args, &block)
+      method_name = InstanceExecMethods.mutex.synchronize do
+        n = 0
+        n += 1 while respond_to?(method_name = "__instance_exec#{n}")
+        InstanceExecMethods.module_eval { define_method(method_name, &block) }
+        method_name
+      end
+
+      begin
+        send(method_name, *args)
+      ensure
+        InstanceExecMethods.module_eval { remove_method(method_name) } rescue nil
+      end
     end
   end
 end
